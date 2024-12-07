a1a89fd7655703cee8ffbb88319a67afb58a01de
login <nikolovi155@gmail.com>
2024-12-07 16:42:05 +0200

    Array

---
+++ HomeWork2/Array.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+class Array {
+private:
+    int* data;
+    size_t size;
+
+    void clear();
+
+public:
+    Array();
+    Array(size_t n);
+    Array(size_t n, int min, int max);
+    Array(size_t n, int x);
+    Array(const Array& other);
+    ~Array();
+
+    void display() const;
+    void fillRandom(int min, int max);
+    void resize(size_t newSize);
+    void sort();
+    int getMin() const;
+    int getMax() const;
+};
--- HomeWork2/Main.cpp
+++ HomeWork2/Main.cpp
@@ -1,20 +1,29 @@
-#include <iostream>
-#include "String.h"
+﻿#include <iostream>
+#include "Array.h"
 
 int main() {
-    String defaultString;
-    defaultString.input();
-    defaultString.output();
+    Array arr1(5);
+    std::cout << "Empty array: ";
+    arr1.display();
 
-    String customString(50);
-    customString.input();
-    customString.output();
+    Array arr2(10, 2, 100);
+    std::cout << "Random array: ";
+    arr2.display();
 
-    String initializedString("Hello, World!");
-    initializedString.output();
+    Array arr3(7, 42);
+    std::cout << "Array with value 42: ";
+    arr3.display();
 
-    String copiedString(initializedString);
-    copiedString.output();
+    arr2.sort();
+    std::cout << "Sorted array: ";
+    arr2.display();
+
+    std::cout << "Min: " << arr2.getMin() << std::endl;
+    std::cout << "Max: " << arr2.getMax() << std::endl;
+
+    arr2.resize(15);
+    std::cout << "Resized array: ";
+    arr2.display();
 
     return 0;
 }
