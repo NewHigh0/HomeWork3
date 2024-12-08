b083cd2c7a9285cd6264eb574d9f4c4f17c1dbe4
login <nikolovi155@gmail.com>
2024-12-08 11:03:11 +0200

    Reservoir

--- HomeWork2/Main.cpp
+++ HomeWork2/Main.cpp
@@ -1,29 +1,16 @@
-﻿#include <iostream>
-#include "Array.h"
+﻿#include "Reservoir.h"
+#include <iostream>
 
-int main() {
-    Array arr1(5);
-    std::cout << "Empty array: ";
-    arr1.display();
+void main()
+{
+    char name[30] = "Reserv";
+    int n = 5;
+    Reservoir* a = new Reservoir[n];
+    a[2].Add(name);
+    a[2].Show();
 
-    Array arr2(10, 2, 100);
-    std::cout << "Random array: ";
-    arr2.display();
-
-    Array arr3(7, 42);
-    std::cout << "Array with value 42: ";
-    arr3.display();
-
-    arr2.sort();
-    std::cout << "Sorted array: ";
-    arr2.display();
-
-    std::cout << "Min: " << arr2.getMin() << std::endl;
-    std::cout << "Max: " << arr2.getMax() << std::endl;
-
-    arr2.resize(15);
-    std::cout << "Resized array: ";
-    arr2.display();
-
-    return 0;
+    a[2].Volume();
+    a[2].Area();
+    a[2].Srav();
+    a[2].~Reservoir();
 }
---
+++ HomeWork2/Reservoir.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+class Reservoir
+{
+    char* name = new char[50];
+    int length;
+    int width;
+    int depth;
+public:
+    void Add(char*);
+    void Show();
+    void Volume();
+    void Area();
+    void Srav();
+
+    Reservoir(const Reservoir& obj);
+
+    Reservoir();
+    ~Reservoir();
+};
